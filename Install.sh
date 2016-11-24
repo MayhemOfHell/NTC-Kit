98a28cca107880004197c2aa9124a458993ac314
MayhemOfHell <bradleymathie@hotmail.com>
2016-11-24 12:54:00 +1030

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -2,4 +2,15 @@
 
 clear
 
-echo "MayDev - Ubuntu 14.04 Setup For NTC Chip Compiling & Flashing"
+echo 
+echo 
+echo "  MayDev - Ubuntu 14.04 Setup For NTC Chip Compiling & Flashing"
+echo 
+echo "   - Step 1: Update Local Ubuntu  "
+sudo apt-get update
+
+echo 
+echo "   - Step 2: Install required packages"
+sudo apt-get install u-boot-tools android-tools-fastboot git build-essential curl android-tools-fsutils libusb-1.0-0-dev pkg-config libncurses5-dev libc6-i386 lib32stdc++6 lib32z1 libusb-1.0-0:i386
+
+
