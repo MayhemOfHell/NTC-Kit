265b950e2ab3221f438eeba4235d37c38daaa73d
Bradley Mathie <MayhemOfHell@users.noreply.github.com>
2016-11-26 21:20:22 +1030

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Check that script is not being run as sudo
 if [ ! "$EUID" -ne 0 ]; then
     echo
@@ -8,9 +7,6 @@ if [ ! "$EUID" -ne 0 ]; then
     echo
     exit 1
 fi
-
-
-# STEP 1 - Load Config.cfg and verify it looks correct
 if [ ! -f Config.cfg ]; then
     echo
     echo " - Unable to locate Config.cfg, make sure you are executing Build.sh in the directory that it exists in"
@@ -20,23 +16,26 @@ else
     source Config.cfg
 fi
 
+# Enable Cache, Change Toolchain To Buildroot
 
-clear   # Clear The Screen - I hate losing my placing when searching over my console output
-
+cd "linux"
+if [ ! -f ".config" ]; then
+    make "ARCH="$ntckit_arc "CROSS_COMPILE="$ntckit_cross "LOCALVERSION="$ntckit_suffix -j $ntckit_cores defconfig
+fi
+#make menuconfig
+#make "ARCH="$ntckit_arc "CROSS_COMPILE="$ntckit_cross "LOCALVERSION="$ntckit_suffix -j $ntckit_cores
+cd ".."
 
 cd "buildroot"
 if [ ! -f ".config" ]; then
     make "chip_defconfig"
 fi
-cd "linux"
-if [ ! -f ".config" ]; then
-    make "defconfig"
-fi
-#make ARCH="$ntckit_arc" "CROSS_COMPILE=$ntckit_cross" menuconfig "-j $ntckit_cores"
-#make ARCH="$ntckit_arc" "CROSS_COMPILE=$ntckit_cross" "LOCALVERSION=$ntckit_suffix" "-j $ntckit_cores"
-#build modules
-#make ARCH="$ntckit_arc" "CROSS_COMPILE=$ntckit_cross" INSTALL_MOD_PATH=../modules
+make menuconfig
+#toolchain type = buildroot
+make
+#http://dn.odroid.com/toolchains/
 cd ".."
+
 
 
 echo " - Build complete"
